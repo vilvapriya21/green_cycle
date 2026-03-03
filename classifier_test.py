3dd472ac399c41931a4190f23f760ba295985189
vilvapriyakoma <vilvapriyakoma@gmail.com>
2026-03-03 17:20:14 +0530

    removed local test file

--- classifier_test.py
+++
@@ -1,8 +0,0 @@
-from app.ml.classifier import WasteClassifier
-
-clf = WasteClassifier()
-print(clf.predict("old paint can"))
-print(clf.predict("banana peel"))
-print(clf.predict("plastic water bottle"))
-print(clf.predict("onion peel"))
-print(clf.predict("plastic cover"))
